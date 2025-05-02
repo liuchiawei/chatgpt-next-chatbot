3799cdf2051703a5e2a7354a818c2ec3a818e0bd
LIU CHIAWEI <doublecheap@gmail.com>
2025-05-02 11:18:13 +0900

    feature(ui): init shadcn add button, alert-dialog, card, chart, file-upload(aceternity), input, separator, sheet, sidebar, skeleton, textarea, tooltip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,9 +21,9 @@ export default function Chat() {
       {/* User Input Block */}
       <form onSubmit={handleSubmit}>
         <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-4 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="質問を入力してください"
           onChange={handleInputChange}
         />
       </form>
