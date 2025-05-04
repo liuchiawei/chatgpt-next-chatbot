bb7820b3487e2ecf9017d60e1cfdef16fe3c0f35
LIU CHIAWEI <doublecheap@gmail.com>
2025-05-04 11:40:34 +0900

    fix(deploy): delete /chat for demo deploy

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,9 @@
 import { openai } from "@ai-sdk/openai";
-import { streamText, tool } from "ai";
+import {
+  streamText,
+  tool,
+  experimental_generateImage as generateImage,
+} from "ai";
 import { z } from "zod";
 
 export const maxDuration = 30;
@@ -15,33 +19,20 @@ export async function POST(req: Request) {
       "You write simple, clear, and concise Japanese content.",
     // prompt: `Summarize the following article in 3-5 sentences: ${article}`,
     tools: {
-      weather: tool({
-        description: "Get the weather in a location (fahrenheit)",
-        parameters: z.object({
-          location: z.string().describe("The location to get the weather for"),
-        }),
-        execute: async ({ location }) => {
-          const temperature = Math.round(Math.random() * (90 - 32) + 32);
-          return {
-            location,
-            temperature,
-          };
-        },
-      }),
-      convertFahrenheitToCelsius: tool({
-        description: "Convert a temperature in fahrenheit to celsius",
-        parameters: z.object({
-          temperature: z
-            .number()
-            .describe("The temperature in fahrenheit to convert"),
-        }),
-        execute: async ({ temperature }) => {
-          const celsius = Math.round((temperature - 32) * (5 / 9));
-          return {
-            celsius,
-          };
-        },
-      }),
+      // generateImageTool: tool({
+      //   description: "Generate an image based on a prompt",
+      //   parameters: z.object({
+      //     prompt: z.string().describe("The prompt to generate an image for"),
+      //   }),
+      //   execute: async ({ prompt }) => {
+      //     const image = await generateImage({
+      //       model: openai("dall-e-3"),
+      //       prompt,
+      //       maxImagesPerCall: 1,
+      //     });
+      //     return { image };
+      //   },
+      // }),
     },
   });
 
